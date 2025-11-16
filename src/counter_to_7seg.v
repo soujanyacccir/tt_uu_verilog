da12537b265433829a57d7c812a3faa0af9051bb
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-16 23:05:59 +0530

    Update counter_to_7seg.v

--- src/counter_to_7seg.v
+++ src/counter_to_7seg.v
@@ -1,20 +1,22 @@
-module counter_to_7seg(
+module counter_to_7seg (
     input  wire [3:0] val,
     output reg  [6:0] seg
 );
+
     always @(*) begin
-        case(val)
-            4'h0: seg = 7'b1111110;
-            4'h1: seg = 7'b0110000;
-            4'h2: seg = 7'b1101101;
-            4'h3: seg = 7'b1111001;
-            4'h4: seg = 7'b0110011;
-            4'h5: seg = 7'b1011011;
-            4'h6: seg = 7'b1011111;
-            4'h7: seg = 7'b1110000;
-            4'h8: seg = 7'b1111111;
-            4'h9: seg = 7'b1111011;
-            default: seg = 7'b0000000;
+        case (val)
+            4'h0: seg = 7'b1000000;
+            4'h1: seg = 7'b1111001;
+            4'h2: seg = 7'b0100100;
+            4'h3: seg = 7'b0110000;
+            4'h4: seg = 7'b0011001;
+            4'h5: seg = 7'b0010010;
+            4'h6: seg = 7'b0000010;
+            4'h7: seg = 7'b1111000;
+            4'h8: seg = 7'b0000000;
+            4'h9: seg = 7'b0010000;
+            default: seg = 7'b1111111;
         endcase
     end
+
 endmodule
