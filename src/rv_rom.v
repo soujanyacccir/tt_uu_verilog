b3c9b3d9990bcb37868bd6053514248f6f63a6f5
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-16 22:51:27 +0530

    Refactor rv_rom module to reduce address width

--- src/rv_rom.v
+++ src/rv_rom.v
@@ -1,19 +1,14 @@
-// rv_rom.v
-`default_nettype none
-module rv_rom #(
-    parameter WORDS = 4096
-) (
-    input  wire         clk,
-    input  wire [11:0]  addr,   // word address
-    output reg  [31:0]  rdata
+module rv_rom (
+    input  wire        clk,
+    input  wire [5:0]  addr,
+    output reg  [31:0] data
 );
-    reg [31:0] mem [0:WORDS-1];
+    reg [31:0] mem[0:63];
+
     initial begin
         $readmemh("firmware.hex", mem);
     end
 
-    always @(posedge clk) begin
-        rdata <= mem[addr];
-    end
+    always @(posedge clk)
+        data <= mem[addr];
 endmodule
-`default_nettype wire
