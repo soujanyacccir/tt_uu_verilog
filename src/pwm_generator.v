5782dc6b174933bdd671c5aa326a5db25eed0b01
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-16 23:05:40 +0530

    Update pwm_generator.v

--- src/pwm_generator.v
+++ src/pwm_generator.v
@@ -1,19 +1,21 @@
 module pwm_generator (
     input  wire clk,
-    input  wire reset,
+    input  wire rst,
     input  wire [7:0] duty,
     output reg  pwm_out
 );
 
     reg [7:0] counter;
 
-    always @(posedge clk) begin
-        if (reset) begin
+    always @(posedge clk or posedge rst) begin
+        if (rst) 
             counter <= 0;
-            pwm_out <= 0;
-        end else begin
+        else 
             counter <= counter + 1;
-            pwm_out <= (counter < duty);
-        end
     end
+
+    always @(*) begin
+        pwm_out = (counter < duty);
+    end
+
 endmodule
