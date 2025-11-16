b19cd0ce79547a5cf570a6dbdc37bb5060d1c6e3
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-16 23:19:51 +0530

    Update rv_rom.v

--- src/rv_rom.v
+++ src/rv_rom.v
@@ -1,22 +1,14 @@
-module simple_ram (
-    input  wire clk,
-    input  wire rst_n,
-    input  wire we,
-    input  wire [7:0] addr,
-    input  wire [7:0] wdata,
-    output reg  [7:0] rdata
+module rv_rom (
+    input  wire        clk,
+    input  wire [5:0]  addr,
+    output reg  [31:0] data
 );
+    reg [31:0] mem[0:63];
 
-reg [7:0] mem [0:255];
-
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n)
-        rdata <= 8'h00;
-    else begin
-        if (we)
-            mem[addr] <= wdata;
-        rdata <= mem[addr];
+    initial begin
+        $readmemh("firmware.hex", mem);
     end
-end
 
+    always @(posedge clk)
+        data <= mem[addr];
 endmodule
