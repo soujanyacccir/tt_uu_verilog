2845a8644de498c957bd9750e67d69511e0ac8b8
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-17 10:18:05 +0530

    Update counter_to_7seg.v

--- src/counter_to_7seg.v
+++ src/counter_to_7seg.v
@@ -1,8 +1,22 @@
 module counter_to_7seg (
     input  wire [3:0] val,
-    output wire [6:0] seg
+    output reg  [6:0] seg
 );
 
-assign seg = 7'b0000000; // dummy combinational (replace if needed)
+always @(*) begin
+    case (val)
+        4'h0: seg = 7'b0111111;
+        4'h1: seg = 7'b0000110;
+        4'h2: seg = 7'b1011011;
+        4'h3: seg = 7'b1001111;
+        4'h4: seg = 7'b1100110;
+        4'h5: seg = 7'b1101101;
+        4'h6: seg = 7'b1111101;
+        4'h7: seg = 7'b0000111;
+        4'h8: seg = 7'b1111111;
+        4'h9: seg = 7'b1101111;
+        default: seg = 7'b0000000;
+    endcase
+end
 
 endmodule
