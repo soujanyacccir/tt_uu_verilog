6ccfa18e3f7c3446e28322be8ced3d3bc2397d19
soujanyacccir <soujanyabharatraj@gmail.com>
2025-11-17 10:52:02 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -12,16 +12,16 @@ module tb;
     wire [7:0] uio_oe;
     reg        ena;
 
-    tt_top dut (
-        .ui_in  (ui_in),
-        .uo_out (uo_out),
-        .uio_in (uio_in),
-        .uio_out(uio_out),
-        .uio_oe (uio_oe),
-        .ena    (ena),
-        .clk    (clk),
-        .rst_n  (rst_n)
-    );
+   tt_um_riscv_core_top dut (
+    .ui_in  (ui_in),
+    .uo_out (uo_out),
+    .uio_in (uio_in),
+    .uio_out(uio_out),
+    .uio_oe (uio_oe),
+    .ena    (ena),
+    .clk    (clk),
+    .rst_n  (rst_n)
+);
 
     always #5 clk = ~clk;
 
